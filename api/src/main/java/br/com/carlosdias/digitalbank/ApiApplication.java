32df65010ea42e41b670fd45e5e5e253701ecdc1
CARLOS AUGUSTO DIAS <carlos.augusto.dias1@gmail.com>
2026-04-11 23:27:12 -0300

    initializing multi-module config

--- api/src/main/java/br/com/carlosdias/digitalbank/ApiApplication.java
+++ api/src/main/java/br/com/carlosdias/digitalbank/ApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DigitalBankApplication {
+public class ApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(DigitalBankApplication.class, args);
+        SpringApplication.run(ApiApplication.class, args);
     }
 
 }
